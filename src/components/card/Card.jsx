e33bf485e5084731c9e4f406ee69b29d16f9fe1c
itzhak12380 <iascc12380@gmail.com>
2022-08-01 16:06:06 +0300

    removed button on card of serive component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,18 +1,16 @@
-import React,{useContext} from 'react'
-import './Card.css'
-import {themContext} from '../../context/contaxt'
-function Card({emoji,heading,detali}) {
-
-  const them = useContext(themContext)
-  const darkMode = them.state.darkMode
+import React, { useContext } from "react";
+import "./Card.css";
+import { themContext } from "../../context/contaxt";
+function Card({ emoji, heading, detali }) {
+  const them = useContext(themContext);
+  const darkMode = them.state.darkMode;
   return (
-    <div className='card' style={darkMode?{color:'white'}:{color:''}}>
-    <img src={emoji} alt="" />
-    <span>{heading}</span>
-    <span>{detali}</span>
-    <button className='c-button'>learn more</button>
+    <div className="card" style={darkMode ? { color: "white" } : { color: "" }}>
+      <img src={emoji} alt="" />
+      <span>{heading}</span>
+      <span>{detali}</span>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
